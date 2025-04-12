b1bfd056e06cb14c160e99bc6046bad5af515536
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-04-12 22:16:26 +0400

    added section about

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const aboutSlider = new Swiper(".about__slider", {
+    loop: true,
+    spaceBetween: 12,
+    slidesPerView: "auto",
+});
+
+const aboutSliderTwo = new Swiper(".about__slider-two", {
+    loop: true,
+    spaceBetween: 12,
+    slidesPerView: "auto",
+});
