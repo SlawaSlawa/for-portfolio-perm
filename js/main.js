aa6f71c9f32b3c11fd9ba4b0129cd2b52963ee4c
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-04-13 20:56:09 +0400

    added section sheme & @media

--- js/main.js
+++ js/main.js
@@ -13,8 +13,67 @@ const aboutSlider = new Swiper(".about__slider", {
     },
 });
 
-const aboutSliderTwo = new Swiper(".about__slider-two", {
+const aboutSliderTwo = new Swiper(".about__slider-two .about__slider", {
     loop: true,
     spaceBetween: 12,
     slidesPerView: "auto",
+});
+
+// const shemeSlider = new Swiper(".sheme__slider", {
+//     slidesPerView: 1,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         type: "fraction",
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+var shemeSlider = null;
+var mediaQuerySize = 768;
+
+function shemeSliderInit() {
+    if (!shemeSlider) {
+        console.log("new Swiper");
+        shemeSlider = new Swiper(".sheme__slider", {
+            slidesPerView: 1,
+            pagination: {
+                el: ".swiper-pagination",
+                type: "fraction",
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    }
+}
+
+function shemeSliderDestroy() {
+    if (shemeSlider) {
+        console.log("destroy");
+
+        shemeSlider.destroy();
+        shemeSlider = null;
+    }
+}
+
+window.addEventListener("load", function () {
+    var windowWidth = this.innerWidth;
+    if (windowWidth <= mediaQuerySize) {
+        shemeSliderInit();
+    } else {
+        shemeSliderDestroy();
+    }
+});
+
+window.addEventListener("resize", function () {
+    var windowWidth = this.innerWidth;
+    if (windowWidth < mediaQuerySize) {
+        shemeSliderInit();
+    } else {
+        shemeSliderDestroy();
+    }
 });
