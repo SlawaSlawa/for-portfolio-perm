4b93ad27121ff097c8ae9178d57a60fcde9302b9
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-04-13 12:14:27 +0400

    added @media for section about

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const aboutSlider = new Swiper(".about__slider", {
     loop: true,
     spaceBetween: 12,
     slidesPerView: "auto",
+    slidesPerView: 1,
+    breakpoints: {
+        320: {
+            slidesPerView: "auto",
+        },
+        768: {
+            slidesPerView: 4,
+        },
+    },
 });
 
 const aboutSliderTwo = new Swiper(".about__slider-two", {
